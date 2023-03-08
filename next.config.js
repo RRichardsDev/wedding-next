a9b7454e6e6fd93e08a506a6a06f45a9cf56718c
Rhodri Richards <rhodri.devlopment@gmail.com>
2023-03-08 00:16:55 +0000

    try with pnpm build

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
   webpack: (config, { isServer }) => {
     // Fixes npm packages that depend on `fs` module
     if (!isServer) {
-      config.node = {
+      config.node = { 
         fs: 'empty'
       }
     }
